d0bb455924c2f41ad83ef782c4a9dffe58d81c42
h4de5ing <lex109@qq.com>
2025-09-04 17:59:52 +0800

    增加socket输入法

---
+++ app/src/main/java/com/android/shell2/services/SocketIME.java
@@ -0,0 +1,115 @@
+package com.android.shell2.services;
+
+import android.inputmethodservice.InputMethodService;
+import android.net.LocalSocket;
+import android.net.LocalSocketAddress;
+import android.os.Build;
+import android.view.inputmethod.CursorAnchorInfo;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
+
+import java.io.DataOutputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+public class SocketIME extends InputMethodService {
+    private LocalSocket mLocalSocket = null;
+    private OutputStream mOutputStream = null;
+    private DataOutputStream mWriter = null;
+    private Thread mThread = null;
+    private static final String SOCKET_NAME = "socket-ime";
+
+    @Override
+    public void onCreate() {
+        if (mThread != null && mThread.isAlive()) {
+            mThread.interrupt();
+        }
+        mThread = new Thread(() -> {
+            mLocalSocket = new LocalSocket();
+            try {
+                mLocalSocket.connect(new LocalSocketAddress(SOCKET_NAME), 1000);
+                mOutputStream = mLocalSocket.getOutputStream();
+                mWriter = new DataOutputStream(mOutputStream);
+                var inputStream = mLocalSocket.getInputStream();
+                var buffer = new byte[1024];
+                while (true) {
+                    var read = inputStream.read(buffer);
+                    if (read > 0) {
+                        var string = new String(buffer, 0, read, StandardCharsets.UTF_8);
+                        getCurrentInputConnection().commitText(string, 1);
+                    }
+                }
+            } catch (Exception ignored) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                    switchToNextInputMethod(false);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mThread != null && mThread.isAlive()) {
+            mThread.interrupt();
+            mThread = null;
+        }
+        try {
+            if (mLocalSocket != null) {
+                mLocalSocket.close();
+                mLocalSocket = null;
+            }
+        } catch (Exception ignored) {
+        }
+    }
+
+    private static final int MESSAGE_TYPE_START = 0;
+    private static final int MESSAGE_TYPE_UPDATE_CURSOR = 1;
+    private static final int MESSAGE_TYPE_STOP = 2;
+
+    @Override
+    public void onStartInput(EditorInfo info, boolean restarting) {
+        try {
+            if (mWriter != null) {
+                mWriter.writeInt(MESSAGE_TYPE_START);
+                mWriter.writeInt(info.inputType);
+                CharSequence initialSelectedText = null;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                    initialSelectedText = info.getInitialSelectedText(0);
+                }
+                if (initialSelectedText != null) {
+                    var buffer = initialSelectedText.toString().getBytes(StandardCharsets.UTF_8);
+                    mWriter.writeInt(buffer.length);
+                    mWriter.write(buffer, 0, buffer.length);
+                }
+                getCurrentInputConnection().requestCursorUpdates(InputConnection.CURSOR_UPDATE_IMMEDIATE | InputConnection.CURSOR_UPDATE_MONITOR);
+            }
+        } catch (Exception ignored) {
+        }
+    }
+
+    @Override
+    public void onUpdateCursorAnchorInfo(CursorAnchorInfo cursorAnchorInfo) {
+        try {
+            if (mWriter != null) {
+                mWriter.writeInt(MESSAGE_TYPE_UPDATE_CURSOR);
+                var selectionStart = cursorAnchorInfo.getSelectionStart();
+                var rect = cursorAnchorInfo.getCharacterBounds(selectionStart);
+                mWriter.writeInt((int) rect.top);
+                mWriter.writeInt((int) rect.bottom);
+                mWriter.writeInt((int) rect.left);
+                mWriter.writeInt((int) rect.right);
+            }
+        } catch (Exception ignored) {
+        }
+    }
+
+    @Override
+    public void onFinishInput() {
+        super.onFinishInput();
+        try {
+            if (mWriter != null) mWriter.writeInt(MESSAGE_TYPE_STOP);
+        } catch (Exception ignored) {
+        }
+    }
+}
